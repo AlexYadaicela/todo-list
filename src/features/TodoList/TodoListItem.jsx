c6b57bf022b331a51e0b3507d8cc52d116cb84a2
spycrab <wialex121@gmail.com>
2025-08-19 23:30:45 -0400

    feat: allow users to edit existing todos

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,17 +1,45 @@
+import * as React from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+
 function TodoListItem({ todo, onCompleteTodo }) {
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [workingTitle, setWorkingTitle] = React.useState(todo.title);
+
+  const handleCancel = () => {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  };
+
+  const handleEdit = (event) => {
+    setWorkingTitle(event.target.value);
+  };
+
   return (
     <li>
-      <form action="">
-        <input
-          type="checkbox"
-          name={todo.title}
-          id={todo.id}
-          checked={todo.isCompleted}
-          onChange={() => {
-            onCompleteTodo(todo.id);
-          }}
-        />
-        {todo.title}
+      <form>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                name={todo.title}
+                id={todo.id}
+                checked={todo.isCompleted}
+                onChange={() => {
+                  onCompleteTodo(todo.id);
+                }}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
       </form>
     </li>
   );
