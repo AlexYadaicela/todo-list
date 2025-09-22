ad2671be212a92632fa70c5e1be93f2f453f7509
spycrab <wialex121@gmail.com>
2025-09-21 21:51:12 -0400

    commit all files

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 function TextInputWithLabel({ elementId, labelText, value, ref, onChange }) {
+  const StyledInput = styled.input`
+    padding-inline-end: 0.5rem;
+  `;
   return (
     <>
       <label htmlFor={elementId}>{labelText}</label>
-      <input
+      <StyledInput
         type="text"
         value={value}
         ref={ref}
